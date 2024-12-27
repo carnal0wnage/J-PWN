5a68324f8aca9069e7e8168923530ac8b60ae0bf
CG <chris@carnal0wnage.com>
2024-12-26 19:41:52 -0500

    init.py updates

--- vuln_checks/__init__.py
+++ vuln_checks/__init__.py
@@ -7,10 +7,15 @@ from .check_open_servicedesk_login import check_open_servicedesk_login
 from .check_open_servicedesk_signup import check_open_servicedesk_signup
 from .check_servicedesk_info import check_servicedesk_info
 from .check_open_jira_signup import check_open_jira_signup
-from .check_open_project_dashboard import check_open_project_dashboard
-from .check_open_project_filter import check_open_project_filter
+from .check_unauthenticated_popular_dashboard import check_unauthenticated_popular_dashboard
+from .check_unauthenticated_popular_filter import check_unauthenticated_popular_filter
 from .check_unauthorized_user_enumeration import check_unauthorized_user_enumeration
 from .check_cve_2019_3403 import check_cve_2019_3403
+from .check_cve_2019_8449 import check_cve_2019_8449
+from .check_cve_2020_14178 import check_cve_2020_14178
+from .check_cve_2020_14179 import check_cve_2020_14179
+from .check_cve_2020_14181 import check_cve_2020_14181
+from .check_cve_2019_11581 import check_cve_2019_11581 
 
 
 __all__ = [
@@ -23,9 +28,14 @@ __all__ = [
     "check_open_servicedesk_signup",
     "check_servicedesk_info",
     "check_open_jira_signup",
-    "check_open_project_dashboard",
-    "check_open_project_filter",
+    "check_unauthenticated_popular_dashboard",
+    "check_unauthenticated_popular_filter",
     "check_unauthorized_user_enumeration",
     "check_cve_2019_3403",
+    "check_cve_2019_8449",
+    "check_cve_2019_11581",
+    "check_cve_2020_14178",
+    "check_cve_2020_14179",
+    "check_cve_2020_14181",
     # Add more function names here...
 ]
